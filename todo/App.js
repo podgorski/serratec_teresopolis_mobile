db322758e9a2a82a612d6bcfa8563a60356e0a72
Antonio Podgorski <podgorski1895@gmail.com>
2020-10-06 11:08:44 -0300

    finalizado roteamento das telas logadas e não logadas

--- todo/App.js
+++ todo/App.js
@@ -1,19 +1,19 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+
 import React from 'react';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import AppRoutes from './routes/app.routes';
+import Routes from './routes'
+
+
+
 
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <AppRoutes />
-      </NavigationContainer>
-      {/* <Tarefas /> */}
+      <Routes />
     </SafeAreaView>
   );
 }
