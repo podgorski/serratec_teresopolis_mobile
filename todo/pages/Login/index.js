1f28c5a474257669102dc99907f6429f48fff825
Antonio Podgorski <podgorski1895@gmail.com>
2020-10-06 11:51:38 -0300

    logica do login iniciada

--- todo/pages/Login/index.js
+++ todo/pages/Login/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Image } from 'react-native';
+import { Image, ActivityIndicator } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
@@ -12,13 +12,42 @@ import {
 } from './styles'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [carregando, setCarregando] = useState(false);
+
+  function handleSubmit() {
+    setCarregando(true)
+
+    // Auth
+
+    // console.warn(email, password)
+    // setCarregando(false)
+  }
+
   return (
     <Container>
       <Image source={logoImg} />
-      <Input />
-      <Input />
-      <Button>
-        <ButtonText>Acessar</ButtonText>
+      <Input
+        value={email}
+        placeholder="Email"
+        onChangeText={text => setEmail(text)}
+      />
+      <Input
+        value={password}
+        placeholder="Senha"
+        onChangeText={text => setPassword(text)}
+        secureTextEntry={true} />
+      <Button onPress={handleSubmit} disabled={!password || !email}>
+
+        {carregando ?
+          <ActivityIndicator color="#333" />
+          :
+          <ButtonText>Acessar</ButtonText>
+        }
+
+
       </Button>
     </Container>
 
